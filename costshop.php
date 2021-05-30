c17270526961a115b1c48a2920ef3169c8857e5f
Nguyễn Duy Phong <pn445522@gmail.com>
2021-05-30 23:49:13 +0700

    Update costshop.php

--- costshop.php
+++ costshop.php
@@ -1,5 +1,4 @@
 {
-    "0":17000,
     "0":18000,
     "access_token":"AQXzQgKTpTSjs-qiBh30aMgm3_Kb53oIf-VA733BpAogVE5jpz3jujU65WJ1XXSvVm1xr2LslGLLCWTNV5Kd_8J1YUx26axkt1E-vsOdvUAgMFH1VJwtclAXdaxRxk5UtmCWeISB6rx6NtvDt7yohnaarpBJjHWMsWYtpNn6nD87n0syud0"
 } 
